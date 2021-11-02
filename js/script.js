899b753b9cb26f49e6316bc0a76a80b2b31fca29
simonalejo1001 <91163272+simonalejo1001@users.noreply.github.com>
2021-11-01 23:21:34 -0500

    Cambios

--- js/script.js
+++ js/script.js
@@ -63,6 +63,31 @@ reset.onclick = function (e) {
 function limpiar(){
     resultado.textContent = "";
   }
+  function resetear(){
+    resultado.textContent = "";
+    operandoa = 0;
+    operandob = 0;
+    operacion = "";
+  }
+  function resolver(){
+    var res = 0;
+    switch(operacion){
+      case "+":
+        res = parseFloat(operandoa) + parseFloat(operandob);
+        break;
+      case "-":
+          res = parseFloat(operandoa) - parseFloat(operandob);
+          break;
+      case "*":
+        res = parseFloat(operandoa) * parseFloat(operandob);
+        break;
+      case "/":
+        res = parseFloat(operandoa) / parseFloat(operandob);
+        break;
+    }
+    resetear();
+    resultado.textContent = res;
+  }
 suma.onclick = function (e) {
     operandoa = resultado.textContent;
     operacion = "+";
